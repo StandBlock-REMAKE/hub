19d051f935916f52ae78cfe5f157a57dd119829d
Cyril COLINET <cyrilcolinet.pro@gmail.com>
2017-08-17 00:46:06 +0200

    Classe principale

--- src/main/java/net/mrlizzard/standblock/remake/hub/StandBlockHUB.java
+++ src/main/java/net/mrlizzard/standblock/remake/hub/StandBlockHUB.java
@@ -1,6 +1,10 @@
 package net.mrlizzard.standblock.remake.hub;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
 
 /**
  * @author MrLizzard
@@ -11,12 +15,33 @@ public class StandBlockHUB extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        if(!getDataFolder().exists())
+            getDataFolder().mkdir();
 
+        File mapConfig = new File(getDataFolder(), "map.yml");
+
+        if(!mapConfig.exists()) {
+            consoleLog("§7Fichier de configuration de map inexistant. Création en cours...");
+            mapConfig.mkdir();
+            // TODO: Copie du contenu se trouvant dans resources/map.yml dans les sources du plugin (try/catch)
+            consoleLog("§7Fichier de configuration de map créé avec succès !");
+        }
+
+        consoleLog("§fPlugin créé à l'occasion d'un évènement public pour StandBlock.");
+        consoleLog("§fPlus d'informations à cette adresse: §bhttps://github.com/StandBlock-REMAKE/hub");
+    }
+
+    public void consoleLog(String message) {
+        getServer().getConsoleSender().sendMessage("§f[§2" + getDescription().getName() + "§f] §r" + message);
     }
 
     @Override
     public void onDisable() {
+        consoleLog("§7Kick des joueurs encore présents (sécurité supplémentaire)");
+        Bukkit.getOnlinePlayers().forEach(player -> player.kickPlayer("§cFermeture du serveur."));
 
+        consoleLog("§7Nettoyage des entitées présentes sur la map...");
+        Bukkit.getWorlds().forEach(world -> world.getEntities().clear());
     }
 
 }
---
+++ src/main/java/net/mrlizzard/standblock/remake/hub/config/MapConfiguration.java
@@ -0,0 +1,7 @@
+package net.mrlizzard.standblock.remake.hub.config;
+
+/**
+ * Created by Lizzard_ on 17/08/2017.
+ */
+public class MapConfiguration {
+}
