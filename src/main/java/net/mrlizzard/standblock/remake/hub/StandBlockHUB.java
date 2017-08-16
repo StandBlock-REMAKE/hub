f0f849f4ef323e70e64ec25c1ee7fc6ba1cb3b26
Cyril COLINET <cyrilcolinet.pro@gmail.com>
2017-08-16 22:28:26 +0200

    First class

---
+++ src/main/java/net/mrlizzard/standblock/remake/hub/StandBlockHUB.java
@@ -0,0 +1,22 @@
+package net.mrlizzard.standblock.remake.hub;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author MrLizzard
+ * @version 0.1-SNAPSHOT
+ * @licence GNU General Public Licence v3.0
+ */
+public class StandBlockHUB extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
